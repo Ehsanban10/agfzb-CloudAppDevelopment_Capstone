d0c4a7e857fc734108fcda82fc11c59eb13c8b1f
Ehsanban10 <138621622+Ehsanban10@users.noreply.github.com>
2023-09-01 22:08:07 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,54 +1,57 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from datetime import datetime
-import logging
-import json
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
+# Import any models and restapis as needed
 
+# Your views for handling user authentication
 
-# Create your views here.
+# View for rendering the registration form
+def registration_view(request):
+    if request.method == "POST":
+        # Handle user registration logic here
+        # You'll need to validate the form data, create a User object, and log the user in
+        # After successful registration, you can redirect the user to a dashboard or another page
+        # Example code for registration logic:
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # first_name = request.POST['first_name']
+        # last_name = request.POST['last_name']
+        # user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+        # login(request, user)
+        # return redirect('dashboard')  # Replace 'dashboard' with your desired URL pattern name
+    else:
+        return render(request, 'djangoapp/registration.html')
 
+# View for handling user login
+def login_request(request):
+    if request.method == "POST":
+        # Handle user login logic here
+        # You'll need to validate the login credentials and log the user in
+        # Example code for login logic:
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # user = authenticate(request, username=username, password=password)
+        # if user is not None:
+        #     login(request, user)
+        #     return redirect('dashboard')  # Replace 'dashboard' with your desired URL pattern name
+        # else:
+        #     messages.error(request, 'Invalid login credentials')
+    return render(request, 'djangoapp/login.html')
 
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
+# View for handling user logout
+def logout_request(request):
+    logout(request)
+    return redirect('login_request')  # Redirect to the login page
 
+# Your views for dealership-related functionality
+# Include views for displaying dealerships, dealer details, and adding reviews
 
-# Create a `contact` view to return a static contact page
-#def contact(request):
-
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
-
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
-
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
-
-# Update the `get_dealerships` view to render the index page with a list of dealerships
+# Example view for displaying a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
-
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
-
